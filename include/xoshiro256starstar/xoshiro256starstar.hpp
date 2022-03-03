bb97aa9692ada8007a297235d46119dfc4b43570
Buster <buster@buster.me.uk>
2022-03-03 21:03:54 +0000

    Add code

--- include/xoshiro256starstar/xoshiro256starstar.hpp
+++ include/xoshiro256starstar/xoshiro256starstar.hpp
@@ -1,11 +1,152 @@
 #pragma once
 
-#include <string>
+#include <array>
+#include <concepts>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <limits>
+#include <random>
 
-/**
- * @brief Return the name of this header-only library
- */
-inline auto name() -> std::string
-{
-  return "xoshiro256starstar";
+namespace xoshiro256starstar {
+struct seed_from_urbg_t {};
+
+inline constexpr seed_from_urbg_t seed_from_urbg{};
+
+struct alignas(32) xoshiro256starstar {
+  using result_type = std::uint64_t;
+
+  static constexpr auto min() {
+    return std::numeric_limits<std::uint64_t>::min();
+  }
+
+  static constexpr auto max() {
+    return std::numeric_limits<std::uint64_t>::max();
+  }
+
+  inline constexpr std::uint64_t operator()();
+
+  // Constructors
+  template <typename T>
+  xoshiro256starstar(seed_from_urbg_t, T &&other_urbg) requires
+      std::uniform_random_bit_generator<std::remove_cvref_t<T>>;
+  explicit xoshiro256starstar();
+  explicit xoshiro256starstar(std::uint64_t seed);
+  explicit xoshiro256starstar(const std::array<std::byte, 256> &seed);
+
+  // Jump functions
+  xoshiro256starstar jump();
+  xoshiro256starstar long_jump();
+
+private:
+  std::uint64_t m_state[4];
+};
+
+namespace detail {
+inline constexpr auto rotl(const std::uint64_t x, unsigned k) {
+  return (x << k) | (x >> (64u - k));
 }
+
+inline auto splitmix64(std::uint64_t &x) {
+  std::uint64_t z = (x += 0x9e3779b97f4a7c15ull);
+  z = (z ^ (z >> 30u)) * 0xbf58476d1ce4e5b9ull;
+  z = (z ^ (z >> 27u)) * 0x94d049bb133111ebull;
+  return z ^ (z >> 31u);
+}
+
+inline void jmp(xoshiro256starstar &generator, std::uint64_t (&state)[4],
+                const std::uint64_t (&jump_table)[4]) {
+  std::uint64_t temp[4]{};
+  for (auto jump_value : jump_table) {
+    for (unsigned b = 0; b != 64u; ++b) {
+      if (jump_value & (std::size_t(1) << b)) {
+        temp[0] ^= state[0];
+        temp[1] ^= state[1];
+        temp[2] ^= state[2];
+        temp[3] ^= state[3];
+      }
+      generator();
+    }
+  }
+
+  state[0] = temp[0];
+  state[1] = temp[1];
+  state[2] = temp[2];
+  state[3] = temp[3];
+}
+} // namespace detail
+
+inline constexpr std::uint64_t xoshiro256starstar::operator()() {
+  // -- xoshiro256** --
+  const std::uint64_t result = detail::rotl(m_state[1] * 5, 7) * 9;
+  const std::uint64_t temp = m_state[1] << 17u;
+  m_state[2] ^= m_state[0];
+  m_state[3] ^= m_state[1];
+  m_state[1] ^= m_state[2];
+  m_state[0] ^= m_state[3];
+  m_state[2] ^= temp;
+  m_state[3] = detail::rotl(m_state[3], 45u);
+  return result;
+}
+
+inline xoshiro256starstar::xoshiro256starstar(
+    const std::array<std::byte, 256> &seed) {
+  std::memcpy(m_state, &seed, sizeof m_state);
+}
+
+inline xoshiro256starstar::xoshiro256starstar(std::uint64_t seed) {
+  for (auto &x : m_state) {
+    x = detail::splitmix64(seed);
+  }
+}
+
+inline xoshiro256starstar::xoshiro256starstar()
+    : xoshiro256starstar(seed_from_urbg, std::random_device{}) {}
+
+template <typename T>
+inline xoshiro256starstar::xoshiro256starstar(seed_from_urbg_t,
+                                              T &&other_urbg) requires
+    std::uniform_random_bit_generator<std::remove_cvref_t<T>> {
+
+  using other_result_type = typename std::remove_cvref_t<T>::result_type;
+  constexpr auto bytes_per_call = sizeof(other_result_type);
+  constexpr auto calls = (sizeof m_state + bytes_per_call - 1) / bytes_per_call;
+  other_result_type seed[calls];
+  for (auto &value : seed) {
+    value = other_urbg();
+  }
+  std::memcpy(&m_state, &seed, sizeof m_state);
+}
+
+// This is the jump function for the generator. It is equivalent to 2^128
+// calls to operator()(); it can be used to generate 2^128 non-overlapping
+// subsequences for parallel computations.
+inline xoshiro256starstar xoshiro256starstar::jump() {
+  static const std::uint64_t jump_table[] = {
+      0x180ec6d33cfd0abaull,
+      0xd5a61266f0c9392cull,
+      0xa9582618e03fc9aaull,
+      0x39abdc4529b1661cull,
+  };
+  xoshiro256starstar save = *this;
+  detail::jmp(*this, m_state, jump_table);
+  return save;
+}
+
+// This is the long-jump function for the generator. It is equivalent to 2^192
+// calls to operator()(); it can be used to generate 2^64 starting points, from
+// each of which jump() will generate 2^64 non-overlapping subsequences for
+// parallel distributed computations.
+inline xoshiro256starstar xoshiro256starstar::long_jump() {
+  static const std::uint64_t long_jump_table[] = {
+      0x76e15d3efefdcbbfull,
+      0xc5004e441c522fb3ull,
+      0x77710069854ee241ull,
+      0x39109bb02acbe635ull,
+  };
+  xoshiro256starstar save = *this;
+  detail::jmp(*this, m_state, long_jump_table);
+  return save;
+}
+} // namespace xoshiro256starstar
--- test/source/xoshiro256starstar_test.cpp
+++ test/source/xoshiro256starstar_test.cpp
@@ -1,7 +1,105 @@
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <iostream>
+#include <iterator>
+
 #include "xoshiro256starstar/xoshiro256starstar.hpp"
 
-auto main() -> int
-{
-  auto result = name();
-  return result == "xoshiro256starstar" ? 0 : 1;
+const std::array<uint64_t, 4> expected_sequences[]{
+    {
+        13346302149916581598ull,
+        3458137911937826359ull,
+        15013289007574211850ull,
+        862047879559611952ull,
+    },
+    {
+        12247022307613036767ull,
+        6189883159010878364ull,
+        12250510543705388769ull,
+        15376822593055763089ull,
+    },
+    {
+        11920099415707066346ull,
+        14422800180009765440ull,
+        11058914709412537623ull,
+        3686108977655562628ull,
+    },
+    {
+        14646694691296698890ull,
+        15099502338332193061ull,
+        12418408594807792822ull,
+        1546099311609212045ull,
+    },
+    {
+        8965407117812691042ull,
+        6229718707537460892ull,
+        17965388879194096341ull,
+        2509736941055013603ull,
+    },
+    {
+        561345601117264966ull,
+        15261714419321652613ull,
+        12890482397767998458ull,
+        14696349688785942750ull,
+    },
+};
+
+auto sub_test(auto sequence_number, auto &&generator) {
+  static auto test_number = 0;
+  auto n = test_number++;
+  auto expected_sequence = expected_sequences[sequence_number];
+  decltype(expected_sequence) actual_sequence;
+
+  std::ranges::generate(actual_sequence, std::ref(generator));
+  auto ok = actual_sequence == expected_sequence;
+
+  if (!ok) {
+    std::cout << "Sub-test " << n << " (sequence " << sequence_number
+              << ")\nExpected {";
+    std::ranges::copy(expected_sequence,
+                      std::ostream_iterator<std::uint64_t>(std::cout, "ull, "));
+    std::cout << "}\nActual {";
+    std::ranges::copy(actual_sequence,
+                      std::ostream_iterator<std::uint64_t>(std::cout, "ull, "));
+    std::cout << "}\n";
+  }
+
+  return ok;
+}
+
+int main() {
+  using urbg = xoshiro256starstar::xoshiro256starstar;
+  using xoshiro256starstar::seed_from_urbg;
+
+  auto generator0 = urbg{0xdeadbeeffeedbadbull};
+  auto generator1 = generator0.long_jump();
+  auto generator2 = generator0.jump();
+  auto generator3 = urbg{seed_from_urbg, generator0};
+  auto generator4 = generator0;
+
+  const char(&seed_string)[256] =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+      "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut e"
+      "nim ad minim veniam, quis nostrud exercitation ullamco laboris n"
+      "isi ut aliquip ex ea commodo consequat. Duis aute irure dolor i";
+
+#if __cpp_lib_bit_cast >= 201806L
+  auto seed = std::bit_cast<std::array<std::byte, 256>>(seed_string);
+#else
+  std::array<std::byte, 256> seed;
+  std::memcpy(&seed, seed_string, sizeof seed);
+#endif
+
+  return !std::ranges::all_of(
+      std::array{
+          sub_test(0, generator0),
+          sub_test(1, generator1),
+          sub_test(2, generator2),
+          sub_test(3, generator3),
+          sub_test(0, generator4),
+          sub_test(4, generator4),
+          sub_test(5, urbg{seed}),
+      },
+      [](auto ok) { return ok; });
 }
